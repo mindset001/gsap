a4ca8eb1c7d9f980a29cccebebba748ac3ff8b45
Shittu-Moshood <shittumoshoodademola@gmail.com>
2022-09-28 10:13:24 +0100

    gsap

---
+++ script/index.js
@@ -0,0 +1,102 @@
+// TweenMax.from('.new-lay',1, {delay: 1, opacity:0, ease: Expo.easeInOut})
+
+TweenMax.to(".overlay h1", 2, {
+    delay: 4,
+  opacity: 0,
+  y: -60,
+  ease: Expo.easeInOut
+})
+
+TweenMax.to(".overlay span", 2, {
+  delay: .9,
+  opacity: 0,
+  y: -20,
+  ease: Expo.easeInOut
+})
+
+TweenMax.to(".overlay", 2, {
+  delay: 3,
+  top: "-100%",
+  ease: Expo.easeInOut
+})
+// TweenMax.from('body',1, {delay: 4, opacity:0, ease: Expo.easeInOut})
+TweenMax.from(".ellipse-container", 1, {
+  delay: 5,
+  opacity: 0,
+  top: "-100%",
+  ease: Expo.easeInOut
+})
+
+TweenMax.from(".yellow", 1, {
+  delay: 3.5,
+  opacity: 0,
+  ease: Expo.easeInOut
+})
+
+TweenMax.from(".circle1", 1, {
+  delay: 6.4,
+  opacity: 0,
+  ease: Expo.easeInOut
+})
+
+TweenMax.from(".circle2", 1, {
+  delay: 6.6,
+  opacity: 0,
+  ease: Expo.easeInOut
+})
+
+TweenMax.from(".logo", 1, {
+  delay: 5,
+  opacity: 0,
+  y: -100,
+  ease: Expo.easeInOut
+})
+
+TweenMax.staggerFrom(".menu-links ul li", 1, {
+  delay: 3.2,
+  opacity: 0,
+  x: -100,
+  ease: Expo.easeInOut
+}, 0.08)
+
+TweenMax.from(".scrolldown", 1, {
+  delay: 3.4,
+  opacity: 0,
+  y: 100,
+  ease: Expo.easeInOut
+})
+
+TweenMax.from(".text .title", 1, {
+  delay: 5,
+  opacity: 0,
+  x: 200,
+  ease: Expo.easeInOut
+})
+
+TweenMax.from(".text p", 1, {
+  delay: 5.2,
+  opacity: 0,
+  x: 200,
+  ease: Expo.easeInOut
+})
+
+TweenMax.from(".watchnow", 1, {
+  delay: 5.4,
+  opacity: 0,
+  x: 200,
+  ease: Expo.easeInOut
+})
+
+TweenMax.staggerFrom(".media ul li", 1, {
+  delay: 5,
+  opacity: 0,
+  y: 100,
+  ease: Expo.easeInOut
+}, 0.08)
+
+TweenMax.staggerFrom(".menu ul li", 2, {
+  delay: 3,
+  opacity: 0,
+  y: 100,
+  ease: Expo.easeInOut
+}, 0.08)
